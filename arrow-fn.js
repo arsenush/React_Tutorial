dc323f60e1462a352782e2bc15499c8c18428cbf
anykytenko <anykytenko@intelliarts.com>
2017-05-17 15:29:09 +0300

    Initital commit

---
+++ arrow-fn.js
@@ -0,0 +1,16 @@
+// var names = ["Ars", "Serg", "Dron"];
+//
+// var person = {
+//   name: "Ars",
+//   greet: function () {
+//     names.forEach((name) => {
+//       console.log(this.name + " says hi to " + name);
+//     });
+//   }
+// };
+//
+// person.greet();
+
+
+var add = (a, b) => a+b;
+console.log(add(3,5));
