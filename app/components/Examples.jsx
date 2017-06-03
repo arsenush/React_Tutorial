b8a125a0ee5580af233a67946241da06a796d635
Arsenush <arsen.nykytenko@gmail.com>
2017-06-03 11:42:14 +0300

    Add styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,23 @@
 var React = require('react');
+var {Link} =require('react-router');
 
 var Examples = () => {
   return (
-    <h3>Examples Component</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are some example locations</p>
+      <ol>
+        <li>
+          <Link to="/?location=Lviv">Lviv, UA</Link>
+        </li>
+        <li>
+          <Link to="/?location=Kyiv">Kyiv, UA</Link>
+        </li>
+        <li>
+          <Link to="/?location=Odesa">Odesa, UA</Link>
+        </li>
+      </ol>
+    </div>
   )
 }
 
